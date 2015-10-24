e687dcebb06ebbae489a50620a1b1e293d2099b5
Mac <Mac@macui-imac-6.local>
2015-10-24 18:00:22 +0900

    카메라 확인 체크 후 띄우기

--- Practice05-ImagePicker/ViewController.swift
+++ Practice05-ImagePicker/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBOutlet var imgView: UIImageView!
+	@IBOutlet var infoLabel: UILabel!
+	@IBOutlet var info2Label: UILabel!
+
+
+	@IBAction func openCamera(sender: AnyObject) {
+		
+		let picker = UIImagePickerController()
+		
+		// 카메라 사용가능 여부
+		if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+/*
+			// 전면 카메라 사용가능 여부
+			if UIImagePickerController.isCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Front) {
+				
+			}
+			else {
+				
+			}
+*/
+			picker.sourceType = UIImagePickerControllerSourceType.Camera
+		}
+		else {
+
+			let alert = UIAlertController(title: "", message: "카메라가 없습니다.", preferredStyle: .Alert)
+			alert.addAction((UIAlertAction(title: "확인", style: .Cancel, handler: {(_)
+			
+			})))
+			self.presentViewController(alert, animated: false, completion: nil)
+			
+			picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+		}
+		
+		picker.allowsEditing = false
+		picker.delegate = self
+		
+		self.presentViewController(picker, animated: true, completion: nil)
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+		
+		// 이미지 불러와서 이미지뷰에 보여주기
+		self.imgView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+		
+		self.infoLabel.text = info[UIImagePickerControllerMediaType] as? String
+		
+		let metadata = info[UIImagePickerControllerMediaMetadata]
+		self.info2Label.text = String(metadata?.count)
+		
+		// 창닫기 - 안닫아주면 리스트화면이 그대로 있음.
+		picker.dismissViewControllerAnimated(false, completion: nil)
 	}
-
-
+	
+	func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+		picker.dismissViewControllerAnimated(false, completion: {
+			$0;
+			let alert = UIAlertController(title: "", message: "취소되었습니다", preferredStyle: .Alert)
+			alert.addAction((UIAlertAction(title: "확인", style: .Cancel, handler: nil)))
+			self.presentViewController(alert, animated: false, completion: nil)
+		})
+	}
+	
 }
 
